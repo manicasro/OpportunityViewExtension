5dabb1065c4ab288036c8b74c639297ca62c5549
Jiri Dolezal <dolezj41@fit.cvut.cz>
2025-04-09 11:17:07 +0200

    Set config to dev env

--- src/extensions/config/Config.ts
+++ src/extensions/config/Config.ts
@@ -10,6 +10,6 @@ export const DefaultConfig: IConfig = {
   tenantId: "b213b057-1008-4204-8c53-8147bc602a29",
   opportunityUrl: "https://tmobileczsk--situat.sandbox.lightning.force.com/lightning/cmp/coredt__NavigateTo?c__objectName=Opportunity&c__externalId=",
   leadUrl: "https://tmobileczsk--situat.sandbox.lightning.force.com/lightning/cmp/coredt__NavigateTo?c__objectName=Lead&c__externalId=",
-  siteName: "sites/f-test-zakazky/verejne_zakazky", //tmoZakazky on dev
+  siteName: "sites/tmoZakazky/verejne_zakazky", //tmoZakazky on dev, f-test-zakazky on prod
   keySequence: ["id=/", "RootFolder=/"],
 };
