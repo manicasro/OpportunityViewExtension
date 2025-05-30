a42a9d1182bd97163a7a77056f1cb38cd903f8e8
Jiri Dolezal <dolezj41@fit.cvut.cz>
2025-05-30 13:05:49 +0200

    Adjust config to prod and add some comments

--- src/extensions/config/Config.ts
+++ src/extensions/config/Config.ts
@@ -8,8 +8,10 @@ export interface IConfig {
 
 export const DefaultConfig: IConfig = {
   tenantId: "b213b057-1008-4204-8c53-8147bc602a29",
-  opportunityUrl: "https://tmobileczsk--situat.sandbox.lightning.force.com/lightning/cmp/coredt__NavigateTo?c__objectName=Opportunity&c__externalId=",
-  leadUrl: "https://tmobileczsk--situat.sandbox.lightning.force.com/lightning/cmp/coredt__NavigateTo?c__objectName=Lead&c__externalId=",
-  siteName: "sites/f-test-zakazky/verejne_zakazky", //tmoZakazky on dev, f-test-zakazky on test, f-zakazky on prod
+  // TEST: https://tmobileczsk--situat.sandbox.lightning.force.com/lightning/cmp/coredt__NavigateTo?c__objectName=Opportunity&c__externalId=
+  // PROD: https://tmcz-st.my.salesforce.com/lightning/cmp/coredt__NavigateTo?c__objectName=Opportunity&c__externalId=
+  opportunityUrl: "https://tmcz-st.my.salesforce.com/lightning/cmp/coredt__NavigateTo?c__objectName=Opportunity&c__externalId=",
+  leadUrl: "https://tmcz-st.my.salesforce.com/lightning/cmp/coredt__NavigateTo?c__objectName=Lead&c__externalId=",
+  siteName: "sites/f-zakazky/verejne_zakazky", //tmoZakazky on dev, f-test-zakazky on test, f-zakazky on prod
   keySequence: ["id=/", "RootFolder=/"],
 };
